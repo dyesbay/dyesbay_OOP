0b1518a7bd685e15ab5c6aae67cfee961bfa7079
Dias Yesbay <34118218+ffellie@users.noreply.github.com>
2018-09-19 15:50:51 +0600

    problem 4

--- Lab2/src/com/company/Time.java
+++ Lab2/src/com/company/Time.java
@@ -9,6 +9,36 @@ public class Time {
         this.minute = minute;
         this.second = second;
     }
+    public int getHour () {
+        return hour;
+    }
+    public int getMinute () {
+        return minute;
+    }
+    public int getSecond () {
+        return second;
+    }
+    
+    public void setHour (int hour){
+        this.hour=hour%24;
+    }
+    public void setMinute (int minute){
+        this.minute=minute%60;
+    }
+    public void setSecond (int second){
+        this.second=second%24;
+    }
+    public void add (Time time){
+        int tempM = (this.second + time.getSecond())/60;
+        this.second+=time.getSecond();
+        this.second%=60;
+        int tempH = this.minute + time.getMinute() + tempM;
+        tempH/=60;
+        this.minute+=time.getMinute()+ tempM;
+        this.minute%=60;
+        this.hour+=time.getHour()+tempH;
+        this.hour%=24;
+    }
     String toUniversal(){
         String s = new String();
         if (hour<10)
